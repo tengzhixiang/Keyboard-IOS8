3e43dcd485bfce3fad85ecf13fc80eb3bcec0c27
Tennng <showtzx@163.com>
2014-08-07 18:02:11 +0800

    create project,begin coding!

---
+++ Keyboard-IOS8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Keyboard-IOS8
+//
+//  Created by microdone on 14/8/7.
+//  Copyright (c) 2014年 microdone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
